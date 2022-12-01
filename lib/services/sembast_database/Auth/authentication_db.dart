33663ef708c497ffdc7bbcc5b3c7bd9b16cf18a6
flutterTamauze <Flutter@tamauzeds.com>
2022-12-01 17:59:21 +0200

    task finsh

--- lib/services/sembast_database/Auth/authentication_db.dart
+++ lib/services/sembast_database/Auth/authentication_db.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:pharmatask/Core/constants/constants.dart';
+import 'package:pharmatask/Core/enums/auth_enum.dart';
 import 'package:pharmatask/features/Authentication/data/model/auth_model.dart';
 import 'package:pharmatask/services/sembast_database/sembast.dart';
 import 'package:sembast/sembast.dart';
@@ -13,7 +14,7 @@ class AuthDao implements AuthenticationAbstractDB {
   var store = StoreRef.main();
   @override
   Future insert(AuthModel user) async {
-    if (await checkIfUserExist(user) == false) {
+    if (await checkIfUserExist(user, AuthType.signup) == false) {
       await _userStore.add(await _db, user.toMap()).then((value) {
         log("user added successfully... $value");
       });
@@ -29,25 +30,27 @@ class AuthDao implements AuthenticationAbstractDB {
 
   @override
   Future<void> login(AuthModel user) async {
-    if (await checkIfUserExist(user)) {
+    if (await checkIfUserExist(user, AuthType.login)) {
       await store.record('username').put(await _db, user.username);
     } else {
       throw "User not found";
     }
   }
 
-  Filter containsMapFilter(Map<String, Object?> map) {
-    return Filter.and(
-        map.entries.map((e) => Filter.equals(e.key, e.value)).toList());
-  }
-
-  Future<bool> checkIfUserExist(AuthModel user) async {
- 
-    final finder = Finder(
-        filter: Filter.and([
-      Filter.equals('username', user.username),
-      Filter.equals('password', user.password)
-    ]));
+  Future<bool> checkIfUserExist(AuthModel user, AuthType authType) async {
+    Finder finder;
+    if (authType == AuthType.login) {
+      finder = Finder(
+          filter: Filter.and([
+        Filter.equals('username', user.username),
+        Filter.equals('password', user.password)
+      ]));
+    } else {
+      finder = Finder(
+          filter: Filter.and([
+        Filter.equals('username', user.username),
+      ]));
+    }
     var snapshots = await _userStore.find(await _db, finder: finder);
     log(snapshots.toString());
     if (snapshots.isEmpty) {
